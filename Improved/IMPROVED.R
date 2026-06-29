ffbbe0e2b4adf3d28cbef9906e45e7a6ddaed340
RiccardoFornaroli <64597202+RiccardoFornaroli@users.noreply.github.com>
2026-06-29 16:05:48 +0200

    Add files via upload

---
+++ Improved/IMPROVED.R
@@ -0,0 +1,301 @@
+############################################################
+# 0. GLOBAL SETTINGS
+############################################################
+
+rm(list=ls())
+
+source("Functions.R")
+
+packages <- c("Hmisc","quantreg","fmsb","Amelia","sm")
+
+tau_shape <- seq(0.02, 0.98, 0.02)
+
+taus_floor   <- (2:10)/100
+taus_median  <- (45:55)/100
+taus_ceiling <- (90:98)/100
+
+taus_groups <- list(Floor=taus_floor,
+                    Median=taus_median,
+                    Ceiling=taus_ceiling)
+
+NULL_MODELS <- c(1,2)
+
+eps <- 0.001
+k_folds <- 5
+
+
+############################################################
+# 1. PACKAGES + DATA
+############################################################
+
+tmp <- which(lapply(packages, require, character.only = TRUE)==FALSE)
+if(length(tmp)>0) install.packages(packages[tmp])
+lapply(packages, require, character.only = TRUE)
+
+dati <- read.table("METRICS_2026_LB_OK.csv", header=TRUE, sep=",")
+
+dati[,c(6,7)] <- rm_outlier_15iqr(dati[,c(6,7)])
+dati <- na.omit(dati)
+
+dati$SUB <- factor(dati$SUB)
+
+GROUP <- dati$GROUP
+SUB   <- dati$SUB
+
+
+############################################################
+# 2. METRICS + TRANSFORMATIONS
+############################################################
+
+Metrics <- dati[,10:ncol(dati)]
+
+Variables_ST <- abs(dati[,c(6,7)])
+Variables_ST$VEL[Variables_ST$VEL==0] <- 0.001
+
+fine_tass <- which(names(Metrics)=="Viviparidae")
+Metrics[,1:fine_tass] <- log10(Metrics[,1:fine_tass] + 1)
+
+logit_fun <- function(x) log((x+eps)/(1-x+eps))
+
+Metrics$logit_EPT_prop   <- logit_fun(Metrics$EPT_prop)
+Metrics$logit_OCH_prop   <- logit_fun(Metrics$OCH_prop)
+Metrics$logit_EPT_EPTOCH <- logit_fun(Metrics$EPT_EPTOCH)
+
+Metrics$EPT_abu   <- log10(Metrics$EPT_abu+1)
+Metrics$OCH_abu   <- log10(Metrics$OCH_abu+1)
+Metrics$ABUNDANCE <- log10(Metrics$ABUNDANCE+1)
+
+
+############################################################
+# 3. MODEL ENGINE
+############################################################
+
+Models <- list(
+  
+  LIN  = function(df) rq(VAR ~ INVARy + GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  LOG  = function(df) rq(VAR ~ log10(INVARy) + GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  EXP  = function(df) rq(VAR ~ exp(INVARy) + GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  QUA  = function(df) rq(VAR ~ poly(INVARy,2) + GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  LIN_INT = function(df) rq(VAR ~ INVARy*GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  LOG_INT = function(df) rq(VAR ~ log10(INVARy)*GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  EXP_INT = function(df) rq(VAR ~ exp(INVARy)*GROUP + SUB, tau=tau_shape, method="sfn", data=df),
+  
+  QUA_INT = function(df) rq(VAR ~ poly(INVARy,2)*GROUP + SUB, tau=tau_shape, method="sfn", data=df)
+)
+
+model_names <- names(Models)
+
+
+############################################################
+# 4. CROSSVALIDATION FUNCTION (QUANTILE LOSS)
+############################################################
+
+quantile_loss <- function(y, pred, tau) {
+  mean((tau - (y < pred)) * (y - pred))
+}
+
+
+cv_rq <- function(formula, data, tau_grid, k=5) {
+  
+  folds <- sample(rep(1:k, length.out=nrow(data)))
+  
+  errors <- c()
+  
+  for(i in 1:k){
+    
+    train <- data[folds != i,]
+    test  <- data[folds == i,]
+    
+    fit <- rq(formula, tau=tau_grid, method="sfn", data=train)
+    
+    pred <- predict(fit, newdata=test)
+    
+    err <- mean((test$VAR - pred)^2, na.rm=TRUE)
+    
+    errors <- c(errors, err)
+  }
+  
+  mean(errors)
+}
+
+
+############################################################
+# 5. SHAPE SELECTION (WI + CV)
+############################################################
+
+wiSHAPE <- vector("list", length(Metrics))
+names(wiSHAPE) <- names(Metrics)
+
+cvSHAPE <- vector("list", length(Metrics))
+names(cvSHAPE) <- names(Metrics)
+
+
+for(i in seq_along(Metrics)) {
+  
+  VAR <- Metrics[,i]
+  
+  wiINVAR <- list()
+  cvINVAR <- list()
+  
+  for(j in seq_along(Variables_ST)){
+    
+    INVARy <- Variables_ST[,j]
+    
+    df <- data.frame(VAR, INVARy, GROUP, SUB)
+    df <- na.omit(df)
+    
+    fits <- lapply(Models, function(m) m(df))
+    names(fits) <- model_names
+    
+    wi_score <- mean_wi(fits, tau_shape)
+    
+    # CV only for best WI model (speed optimization)
+    best_formula <- VAR ~ INVARy + GROUP + SUB
+    
+    cv_score <- cv_rq(best_formula, df, tau_shape, k_folds)
+    
+    wiINVAR[[j]] <- wi_score
+    cvINVAR[[j]] <- cv_score
+  }
+  
+  wiSHAPE[[i]] <- wiINVAR
+  cvSHAPE[[i]] <- cvINVAR
+}
+
+
+############################################################
+# 6. VARIABLE SELECTION (WI + CV)
+############################################################
+
+wi_selected <- list()
+
+for(v in seq_along(Metrics)) {
+  
+  sel <- data.frame(
+    Index=1:ncol(Variables_ST),
+    Variable=colnames(Variables_ST),
+    Wi=NA,
+    CV=NA
+  )
+  
+  for(i in 1:ncol(Variables_ST)) {
+    sel$Wi[i] <- wiSHAPE[[v]][[i]][1,1]
+    sel$CV[i] <- cvSHAPE[[v]][[i]]
+  }
+  
+  # composite score: WI high + CV low
+  sel$score <- sel$Wi / (sel$CV + 1e-6)
+  
+  wi_selected[[v]] <- sel
+}
+
+names(wi_selected) <- names(Metrics)
+
+
+############################################################
+# 7. FINAL VARIABLE CHOICE
+############################################################
+
+var_list <- list()
+
+for(v in seq_along(Metrics)) {
+  
+  sel <- wi_selected[[v]]
+  
+  best <- sel[which.max(sel$score),]
+  
+  var_list[[v]] <- data.frame(
+    Variable = best$Variable
+  )
+}
+
+names(var_list) <- names(Metrics)
+
+
+############################################################
+# 8. FINAL MODELS
+############################################################
+
+var_final <- list()
+
+for(v in seq_along(Metrics)) {
+  
+  VAR <- Metrics[,v]
+  
+  if(is.na(var_list[[v]]$Variable)) next
+  
+  INVAR <- var_list[[v]]$Variable
+  
+  df <- data.frame(VAR, Variables_ST, GROUP, SUB)
+  
+  formula <- as.formula(
+    paste("VAR ~", INVAR, "+ GROUP + SUB")
+  )
+  
+  fit <- rq(formula, tau=tau_shape, method="sfn", data=df)
+  
+  cv_err <- cv_rq(formula, df, tau_shape, k_folds)
+  
+  var_final[[v]] <- list(
+    Metric = names(Metrics)[v],
+    Variable = INVAR,
+    Formula = formula,
+    Model = fit,
+    CV = cv_err
+  )
+}
+
+names(var_final) <- names(Metrics)
+
+
+############################################################
+# 9. RESULTS TABLE
+############################################################
+
+results <- data.frame(
+  Metrics = names(Metrics),
+  Variable = sapply(var_final, function(x) x$Variable),
+  CV_error = sapply(var_final, function(x) x$CV)
+)
+
+
+############################################################
+# 10. MODELS FOR PLOTS
+############################################################
+
+modelli_pronti <- list()
+
+for(i in seq_along(Metrics)) {
+  
+  m <- names(Metrics)[i]
+  
+  if(!is.null(var_final[[m]])) {
+    
+    df <- data.frame(VAR=Metrics[,m], Variables_ST, GROUP, SUB)
+    
+    modelli_pronti[[m]] <- tryCatch({
+      
+      rq(var_final[[m]]$Formula,
+         tau=c(0.05,0.5,0.95),
+         method="sfn",
+         data=df)
+      
+    }, error=function(e) NULL)
+  }
+}
+
+
+############################################################
+# 11. SAVE
+############################################################
+
+save(results,
+     var_final,
+     modelli_pronti,
+     file="IMPROVED_quantile_models.RData")
